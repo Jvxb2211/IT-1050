4c21a7eab9bae1a786bd4d1aded37d97c3194369
josephvxbellini@gmail.com <josephvxbellini@gmail.com>
2019-03-26 21:12:56 -0400

    Completed Lab 3

---
+++ IT1050Lab3/IT1050Lab3/Instructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050Lab3
+{
+    class Instructor
+    {
+        public string Name { get; set; }
+        public string CourseName { get; set; }
+
+        public Instructor(string Name, string CourseName)
+        {
+            this.Name = Name;
+            this.CourseName = CourseName;
+        }
+
+        public void SetStudentGrade(Student thisStudent, int thisGrade)
+        {
+            thisStudent.Grade = thisGrade; 
+        }
+
+        public string Print()
+        {
+            return (Name + " | Course : " + CourseName);
+        }
+    }
+}
